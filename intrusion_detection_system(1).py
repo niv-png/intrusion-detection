daea2fb4db10ebe75ce4be6f77523393807b9912
sarakhadr14 <128757315+sarakhadr14@users.noreply.github.com>
2023-03-25 00:29:38 -0500

    Update intrusion_detection_system(1).py

--- intrusion_detection_system(1).py
+++ intrusion_detection_system(1).py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 #n -libraries for the files in google drive
 from pydrive.auth import GoogleAuth
